eed903b746d18199626b7a35d672d921da37f3bb
Sherman Chen <shermchen@paypal.com>
2018-06-17 11:19:11 -0700

    (feat) essentially a to do app at this point

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -48,4 +48,14 @@ defmodule Discuss.TopicController do
         render conn, "edit.html", changeset: changeset, topic: old_topic
     end
   end
+
+  # when you uses resources for routing it expects the "id" to be in the map
+  def delete(conn, %{"id" => topic_id}) do
+    Repo.get!(Topic, topic_id)
+    |> Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic Deleted")
+    |> redirect(to: topic_path(conn, :index))
+  end
 end
