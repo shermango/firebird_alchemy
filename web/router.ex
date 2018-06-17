b45f2297c02651d04eddba069797b85347e87aeb
Sherman Chen <shermchen@paypal.com>
2018-06-17 11:09:59 -0700

    (feat) switch to resources routing

--- web/router.ex
+++ web/router.ex
@@ -15,12 +15,18 @@ defmodule Discuss.Router do
 
   scope "/", Discuss do
     pipe_through :browser # Use the default browser stack
+    
+    # get "/", TopicController, :index
+    # get "/topics/new", TopicController, :new
+    # post "/topics", TopicController, :create
+    # get "/topics/:id/edit", TopicController, :edit
+    # put "/topics/:id", TopicController, :update
 
-    get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
-    get "/topics/:id/edit", TopicController, :edit
-    put "/topics/:id", TopicController, :update
+    # resources is freaking magical, but it breaks restful convention in that it will strip the suffix route
+    # is it better to accept this as a framework level choice
+    # or make a 304 redirect to /topic on landing, etc? food for thought
+
+    resources "/", TopicController
   end
 
   # Other scopes may use custom stacks.
